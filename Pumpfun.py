bb4dc6875976a2ad505711902b04fe3b1b4df1b7
Spencer-Askine <108842805+Spencer-Askine@users.noreply.github.com>
2024-06-25 22:55:40 -0400

    Update Pumpfun.py

--- Pumpfun.py
+++ Pumpfun.py
@@ -50,8 +50,7 @@ embed = DiscordEmbed
 while True:
 
     try:
-        #testresponse = requests.get('https://client-api-2-74b1891ee9f9.herokuapp.com/coins?offset=0&limit=20&sort=created_timestamp&order=desc&includeNsfw=true')
-        #print(testresponse.json())
+        
         response = requests.get('https://frontend-api.pump.fun/coins/latest')# , proxies = proxyrotate)
         time.sleep(0.5)
         addy2 = response.json()['mint']
